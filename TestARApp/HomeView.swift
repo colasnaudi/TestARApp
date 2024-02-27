12a2fa2c01aff03915384ca4b458480ab55915eb
colasnaudi <colasnaudi@gmail.com>
2024-02-27 17:18:22 +0100

    WIP

---
+++ TestARApp/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Socotrap
+//
+//  Created by Colas Naudi on 27/02/2024
+//
+
+import SwiftUI
+import ARKit
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Text("Bienvenue dans l'application AR")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Ceci est un texte explicatif de l'application AR.")
+                    .font(.body)
+                    .padding()
+                
+                Spacer()
+                
+                NavigationLink(destination: SelectFileView()) {
+                    Text("Suivant")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding()
+                Spacer()
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle()) // Pour que la navigationView prenne l'écran entier
+    }
+}
+
+ 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestARApp/Scan.swift
@@ -0,0 +1,8 @@
+//
+//  Scan.swift
+//  TestARApp
+//
+//  Created by Colas Naudi on 27/02/2024.
+//
+
+import Foundation
